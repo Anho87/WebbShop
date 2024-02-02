45dd27aa7ab0287c12000d306d945b75abd515b0
JohanJohnsson <johan.johnsson@yh.nackademin.se>
2024-02-02 11:53:19 +0100

    Added showShoes and printShoesOfCategory

--- src/Webbshop.java
+++ src/Webbshop.java
@@ -1,5 +1,6 @@
 import Repository.Repository;
 import TableClasses.*;
+import com.google.protobuf.StringValue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Webbshop {
-    
+
     private Scanner sc = new Scanner(System.in);
     private List<Customer> customerList;
     private List<OrderedItems> orderedItemsList;
@@ -16,7 +17,7 @@ public class Webbshop {
     private List<Shoe> shoeList;
     private List<Size> sizeList;
     private List<TelephoneNumber> telephoneNumberList;
-    private List<Brand> brandList; 
+    private List<Brand> brandList;
     private List<Category> categoryList;
     private List<CategoryName> categoryNameList;
     private List<Color> colorList;
@@ -34,97 +35,80 @@ public class Webbshop {
 
     public Webbshop() {
 
-        // r.printAllShoes();
-        
-        customerList = r.getCustomerData();
-        //customerList.forEach(e -> System.out.println(e.toString()));
-
-        //System.out.println();
-
-        // r.printAllOrderedItems();
-        orderedItemsList = r.getOrderItemsData();
-        //orderedItemsList.forEach(e -> System.out.println(e.toString()));
-
-        //System.out.println();
-        
-        placedOrderList = r.getPlacedOrderData();
-        //placedOrderList.forEach(e -> System.out.println(e.toString()));
-
-        //System.out.println();
-        
-        shoeList = r.getShoeData();
-        //shoeList.forEach(e -> System.out.println(e.toString()));
-
-        //System.out.println();
-        
-        sizeList = r.getSizeData();
-        //sizeList.forEach(e -> System.out.println(e.toString()));
-
-        //System.out.println();
-        
-        telephoneNumberList = r.getTelephoneNumberData();
-        //telephoneNumberList.forEach(e -> System.out.println(e.toString()));
-
-        //System.out.println();
-        
-        brandList = r.getBrandData();
-        //brandList.forEach(e -> System.out.println(e.toString()));
-
-        //System.out.println();
-        
-        categoryList = r.getCategoryData();
-        //categoryList.forEach(e -> System.out.println(e.toString()));
-
-        //System.out.println();
-        
-        categoryNameList = r.getCategoryNameData();
-        //categoryNameList.forEach(e -> System.out.println(e.toString()));
-
-        //System.out.println();
-        
-        colorList = r.getColorData();
-        //colorList.forEach(e -> System.out.println(e.toString()));
-
-        //shoeList.forEach(e -> System.out.println(e));
-
-        //brandList.stream().filter(e -> e.getBrand(shoeList.stream().filter(f -> f.getStock() > 0))).forEach(e -> System.out.println(e));
-       /* brandList.stream()
-                .filter(brand -> shoeList.stream()
-                        .anyMatch(shoe -> shoe.getBrandId() == brand.getId() && shoe.getStock() > 0))
-                .forEach(e -> System.out.println(e.getBrand()));*/
-        /*placedOrderList.stream().filter(placedOrder -> customerList.stream()
-                .anyMatch(customer -> customer.getId() == placedOrder.getCustomerId() && customer.getFirstName().equalsIgnoreCase("Mayo")))
-                .forEach(e -> System.out.println(e.getId()));*/
+        getAllData();
         login();
+
     }
-    public void login(){
+
+    public void getAllData() {
+        customerList = r.getCustomerData();
+        orderedItemsList = r.getOrderItemsData();
+        placedOrderList = r.getPlacedOrderData();
+        shoeList = r.getShoeData();
+        sizeList = r.getSizeData();
+        telephoneNumberList = r.getTelephoneNumberData();
+        brandList = r.getBrandData();
+        categoryList = r.getCategoryData();
+        categoryNameList = r.getCategoryNameData();
+        colorList = r.getColorData();
+    }
+
+
+    public void login() {
         System.out.println("Välkommen!");
         System.out.print("Skriv in din mailadress: ");
         userName = sc.nextLine().trim();
         System.out.print("Skriv in ditt lösenord: ");
         password = sc.nextLine().trim();
-        for (Customer c: customerList) {
-            if(c.getEmailAddress().equalsIgnoreCase(userName)&& c.getPassword().equalsIgnoreCase(password)){
+        for (Customer c : customerList) {
+            if (c.getEmailAddress().equalsIgnoreCase(userName) && c.getPassword().equalsIgnoreCase(password)) {
                 loggedIn = true;
                 currentCustomer = c;
                 orders();
             }
         }
     }
-    public void orders(){
+
+    public void orders() {
         System.out.println("Välkommen " + currentCustomer.getFirstName());
         System.out.println("Vill du lägga till produkter till en befintlig order eller skapa en ny?");
         System.out.println("1. Skapa en ny order.\n2. Lägg till i en befintlig order.");
         int a = sc.nextInt();
-        if(a == 1){
+        if (a == 1) {
             orderId = 0;
-        }else{
+            showShoes();
+        } else {
             System.out.println("Välj en befintlig order:");
-            placedOrderList.stream().filter(e -> e.getCustomerId() == currentCustomer.getId()).forEach(e-> System.out.println("OrderNr: " + e.getId()));
+            placedOrderList.stream().filter(e -> e.getCustomerId() == currentCustomer.getId()).forEach(e -> System.out.println("OrderNr: " + e.getId()));
             orderId = sc.nextInt();
         }
-        
+
+    }
+
+    public void showShoes() {
+        while (true) {
+            System.out.println("Välj en kategori");
+            for (int i = 1; i < categoryNameList.size() + 1; i++) {
+                for (CategoryName c : categoryNameList) {
+                    if (c.getId() == i) {
+
+                        System.out.println(i + ": " + c.getCategoryName());
+                    }
+                }
+            }
+            int a = sc.nextInt();
+            printShoesOfCategory(a);
+        }
+
+    }
+
+    public void printShoesOfCategory(int a) {
+        brandList.stream()
+                .filter(brand -> categoryList.stream()
+                        .anyMatch(category -> category.getBrandId() == brand.getId() && category.getCategoryNameId() == a))
+                .forEach(e -> System.out.println(e.getBrand()));
     }
 }
+    
         
 
