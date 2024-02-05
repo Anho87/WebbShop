9c189dcdff1b101b0b6acf957fe2a1cf9f97052c
JohanJohnsson <johan.johnsson@yh.nackademin.se>
2024-02-05 10:44:25 +0100

    Added totalSalesamountPerCustomer() and ordersPerCustomer()

--- src/SalesApplication.java
+++ src/SalesApplication.java
@@ -1,8 +1,11 @@
 import Repository.Repository;
 import TableClasses.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class SalesApplication {
     private Scanner sc = new Scanner(System.in);
@@ -33,13 +36,27 @@ public class SalesApplication {
 
     public SalesApplication() {
         getAllData();
-        System.out.println("Skriv in färg: ");
-        String color = sc.next().trim();
-        System.out.println("skriv in storlek");
-        int size = sc.nextInt();
-        System.out.println("Skriv in märke");
-        String brand = sc.next().trim();
-        sales(color, size, brand);
+        System.out.println("Vilken rapport vill du se?");
+        System.out.println("1.\n2.\n3.\n4.\n5.");
+        int choice = sc.nextInt();
+        
+        switch (choice) {
+            case 1: System.out.println("Skriv in färg: ");
+            String color = sc.next().trim();
+            System.out.println("skriv in storlek");
+            int size = sc.nextInt();
+            System.out.println("Skriv in märke");
+            String brand = sc.next().trim();
+            sales(color, size, brand);
+            break;
+            
+            case 2: ordersPerCustomer();
+            break;
+            
+            case 3: totalSalesamountPerCustomer();
+            break;
+        }
+        
     }
 
     public void sales(String inColor, int inSize, String inBrand) {
@@ -63,8 +80,64 @@ public class SalesApplication {
         filteredCustomers.forEach(e -> System.out.println(e.getFirstName() + " " + e.getLastName()
                 + " " + e.getAddress() + " " + e.getPostalCode() + " " + e.getCity()));
     }
+    
+    public void totalSalesamountPerCustomer(){
+        Map<Integer, Integer> customerTotal = new HashMap<>();
+
+        for (PlacedOrder placedOrder : placedOrderList) {
+            for (OrderedItems orderedItems : orderedItemsList) {
+                if (orderedItems.getPlacedOrderId() == placedOrder.getId()) {
+                    Shoe shoe = shoeList.stream()
+                            .filter(s -> s.getId() == orderedItems.getShoeId())
+                            .findFirst()
+                            .orElse(null);
+
+                    if (shoe != null) {
+                        Customer customer = customerList.stream()
+                                .filter(c -> c.getId() == placedOrder.getCustomerId())
+                                .findFirst()
+                                .orElse(null);
+
+                        if (customer != null) {
+                            int orderTotal = customerTotal.getOrDefault(customer.getId(), 0);
+                            orderTotal += shoe.getPrice();
+                            customerTotal.put(customer.getId(), orderTotal);
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("Kundrapport: Totala beställningsvärde");
+        customerTotal.forEach((customerId, totalAmount) -> customerList.stream()
+                .filter(c -> c.getId() == customerId)
+                .findFirst().ifPresent(customer -> System.out.println(customer.getFirstName() + " " + customer.getLastName()
+                        + ": Total summa - " + totalAmount)));
+    
+    }
+    public void ordersPerCustomer() {
+        Map<Integer, Integer> ordersCount = new HashMap<>();
+
+        for (PlacedOrder placedOrder : placedOrderList) {
+            int customerId = placedOrder.getCustomerId();
+            int ordersAmount = ordersCount.getOrDefault(customerId, 0);
+            ordersAmount++;
+            ordersCount.put(customerId, ordersAmount);
+        }
+
+        System.out.println("Kundrapport: Antal ordrar");
+        ordersCount.forEach((customerId, ordersAmount) -> {
+            customerList.stream()
+                    .filter(c -> c.getId() == customerId)
+                    .findFirst().ifPresent(customer -> System.out.println(customer.getFirstName() + " " + customer.getLastName()
+                            + ": Total Orders - " + ordersAmount));
+
+        });
+    }
+    
 
     public static void main(String[] args) {
         new SalesApplication();
     }
 }
+
